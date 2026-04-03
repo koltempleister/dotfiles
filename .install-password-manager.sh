a4bd47bcfc2eddd096b910dcf3adfca8533aecd7
jevdheyd <jensvanderheyden@vubbe>
2026-04-03 09:18:16 +0200

    fix install bw

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -3,7 +3,8 @@
 # exit immediately if password-manager-binary is already in $PATH
 type bw >/dev/null 2>&1 && exit
 
-sudo apt install snap
+sudo apt update
+sudo apt install snapd
 
 echo 'installing bitwarden cli'
 sudo snap install bw
