13520fd44ed022e9b859df3f0ed6cb720f371761
jevdheyd <jensvanderheyden@vubbe>
2026-04-03 09:13:18 +0200

    use pw manager to install private repos

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -2,5 +2,11 @@
 
 # exit immediately if password-manager-binary is already in $PATH
 type bw >/dev/null 2>&1 && exit
+
+sudo apt install snap
+
 echo 'installing bitwarden cli'
 sudo snap install bw
+
+echo 'login'
+export BW_SESSION=$(bw login {{ .bitwardenEmail }} --raw)
