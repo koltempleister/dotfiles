2297f11a7898cca82c0819537a545d1c9397d9e7
jevdheyd <jensvanderheyden@vubbe>
2026-05-29 09:02:42 +0200

    tmux and nvim to erverforest

--- dot_config/nvim/lua/plugins/everforest.lua
+++ dot_config/nvim/lua/plugins/everforest.lua
@@ -9,14 +9,12 @@ return {
     "neanias/everforest-nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      background = "medium", -- Opties: "soft", "medium", "hard"
-      transparent_background_level = 0,
-      italics = false,
-    },
     config = function()
       require("everforest").setup({
         -- Je aangepaste opties hier
+        background = "hard", -- Opties: "soft", "medium", "hard"
+        transparent_background_level = 1,
+        italics = false,
       })
       -- Laad het thema expliciet om conflicten met lazy.nvim te voorkomen
       vim.cmd("colorscheme everforest")
